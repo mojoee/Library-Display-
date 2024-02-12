074943364f39d157bca323a5873582e9f8532039
mojoee <47809126+mojoee@users.noreply.github.com>
2024-02-12 09:20:51 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(){
+    // Constructor
+}
+
+function addBookToLibrary(){
+    // do the logic
+}
+
+function showLibrary(Lib){
+    console.log("The library contains the following books")
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+}
